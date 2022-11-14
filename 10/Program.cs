5b25edacc6aeb11fb9d659bc617f8fa58fa886f2
Kirill <forestblackone@gmail.com>
2022-11-14 22:57:16 +0300

    10 great

---
+++ 10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10 Вывести на экран последнюю цифру целого числа введенного с клавиатуры
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n >= 10){
+    n = n - 10;
+}
+Console.WriteLine(Math.Abs(n));
---
+++ 9/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 9 Вывести на экран четные числа от 1 до N
+
+Console.WriteLine("Введите число N: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+
+while(number <= n){
+    if(number % 2 == 0){
+        Console.Write(number + " ");
+        number++;
+    }
+    else{number++;}
+}
