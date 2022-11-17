6ad12a8012186fdb249499591051021aae2981a2
Kirill <forestblackone@gmail.com>
2022-11-17 13:21:11 +0300

    15 great

---
+++ 15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 15 С клавиатуры вводится целое число. 
+// Вывести третью цифру числа или сообщить, что её нет (Вывести: NO)
+
+Console.WriteLine("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n < 99){
+    Console.WriteLine("NO");
+}
+if(n > 99 && n < 999){
+    Console.WriteLine("третья цифра числа: " + n % 10);
+}
+else{Console.WriteLine("третья цифра числа: " + (n % 100) / 10);}
