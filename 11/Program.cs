9cae24e2e563f866916f91e2b65df20755c2316c
Kirill <forestblackone@gmail.com>
2022-11-17 12:23:32 +0300

    11 great

---
+++ 11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 11 Дано число больше 9. Вывести на экран  вторую цифру числа с конца
+
+Console.WriteLine("Введите число больше 9: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 9){
+int result = (n % 100) / 10;
+Console.WriteLine(result);}
+else{Console.WriteLine("Число меньше 9.");}
