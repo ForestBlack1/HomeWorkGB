858af422595aca2d23895c9781b2e86f0b2d8526
Kirill <forestblackone@gmail.com>
2022-11-17 13:12:34 +0300

    14 great

---
+++ 14/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 14 С клавиатуры вводятся два числа a и b. 
+// Выяснить, кратно ли число a числу b, если нет, вывести остаток от деления a на b.
+
+Console.WriteLine("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a % b == 0){
+    Console.WriteLine("Число а кратно числу b.");
+}
+else{Console.WriteLine("Остаток: " + a % b);}
