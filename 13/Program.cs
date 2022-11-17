33819a25d5462ea8b9aa9e3546b3b658ff1dc0ee
Kirill <forestblackone@gmail.com>
2022-11-17 13:05:47 +0300

    13 great

---
+++ 13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13 Удалить вторую цифру целого числа введенного с клавиатуры.
+
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 9 && n < 99){
+    int number = n % 10;
+    Console.WriteLine("Число " + number);}
+if(n > 99 && n < 999){
+    int first_number = Math.Abs(n / 100);
+    int second_number = n % 10;
+    Console.Write("Число " + first_number);
+    Console.Write(second_number);}
+if(n < 9){
+    Console.WriteLine("Число " + n);
+}
