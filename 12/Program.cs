61a75986784971884b6a5530246b99786e85081e
Kirill <forestblackone@gmail.com>
2022-11-17 12:41:40 +0300

    12 great

---
+++ 12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 12 С клавиатуры вводится целое число из  диапазона  [10, 99]. Показать наибольшую цифру числа
+Console.WriteLine("Введите число в диапазоне [10 - 99]: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n > 10 && n < 99){
+    int firstnumber = (n / 10) % 10;
+    int secondnumber = ((n * 10) % 100) / 10;
+    if(firstnumber > secondnumber){
+        Console.WriteLine("Наибольшая цифра: " + firstnumber);
+    }
+    else{Console.WriteLine("Наибольшая цифра: " + secondnumber);}
+}
+else{Console.WriteLine("Соблюдайте условие.");}
