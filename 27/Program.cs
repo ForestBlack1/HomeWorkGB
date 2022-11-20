af7afa74d9541c0704862b8cff124a0956d9a1a5
Kirill <forestblackone@gmail.com>
2022-11-20 20:12:18 +0300

    27 great

---
+++ 27/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 27 Возведите число А в натуральную степень B используя цикл
+
+int a = 5;
+int b = 3;
+int result = 1;
+for(int i = 1; i <= b; i++){
+    result = result * a;
+}
+Console.WriteLine(result);
