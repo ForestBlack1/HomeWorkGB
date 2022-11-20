0cced90fcd5d944071e104ed333d3bea24fd7812
Kirill <forestblackone@gmail.com>
2022-11-20 20:00:58 +0300

    22 great

---
+++ 22/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 22 Программа проверяет пятизначное число на палиндромом.
+
+string value = "12321";
+
+string Polindrom(string value){
+    string result = String.Empty;
+    string newValue = String.Empty;
+    for(int i = value.Length - 1; i >= 0; i--){
+        char a = value[i];
+        newValue = newValue + $"{a}";
+    }
+    Console.WriteLine(value + " - " + newValue);
+    if(value == newValue){
+        result = "Число является полиндромом.";
+    }
+    else{result = "Число не полиндром.";}
+    return result;
+}
+
+Console.WriteLine(Polindrom(value));
