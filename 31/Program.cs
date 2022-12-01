1bf543a3634a99961bd5f0528440385aa1ae1e9c
Kirill <forestblackone@gmail.com>
2022-12-01 21:46:22 +0300

    31 great

---
+++ 31/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 31 Показать кубы чисел, заканчивающихся на четную цифру
+int n = 100; // предел чисел
+int result = 1;
+for(int i = 1; i <= n; i++){
+    if(i % 2 == 0){
+        result = i * i * i;
+        Console.WriteLine(result);
+    }
+}
