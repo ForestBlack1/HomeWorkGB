8642539fa808a4c7bc5fa7f544330944d27a14ee
Kirill <forestblackone@gmail.com>
2022-12-01 21:35:25 +0300

    30 great

---
+++ 30/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 30 Написать программу вычисления произведения чисел от 1 до N
+
+int n = 10;
+double result = 1;
+for(int i = 2; i <= n; i++){
+    result = result * i;
+}
+Console.WriteLine(result);
