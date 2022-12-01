dca1bfb47427e15b0848c2902b95d6e322a2ce02
Kirill <forestblackone@gmail.com>
2022-12-01 22:08:07 +0300

    33 great

---
+++ 33/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 33 Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+int[] matr = new int[8];
+Random rnd = new Random();
+for(int i = 0; i < matr.Length; i ++){
+    matr[i] = rnd.Next(0, 2);
+    Console.WriteLine(matr[i]);
+}
