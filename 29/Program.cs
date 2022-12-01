74cd178dfd18bb66a5955d003d4cd51760ce7c0f
Kirill <forestblackone@gmail.com>
2022-12-01 21:18:28 +0300

    29 great

---
+++ 29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 29 Подсчитать сумму цифр в числе. Сделать подпрограмму.
+
+int value = 33;
+int result = 0;
+int Sum(int value){
+    if(value < 100){
+        result = (value % 10) + (value / 10);
+    }
+    if(value < 1000){
+        result = (value % 10) + ((value % 100) / 10) + (value / 100);
+    }
+    return result;
+
+}
+Console.WriteLine(Sum(value));
