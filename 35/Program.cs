648221b4aca18a2e6f87e6309c656ecd87808da0
Kirill <forestblackone@gmail.com>
2022-12-01 22:22:36 +0300

    35 great

---
+++ 35/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 35 Написать программу замены элементов массива на противоположные
+
+int[] matr = new int[8];
+Random rnd = new Random();
+for(int i = 0; i < matr.Length; i ++){
+    matr[i] = rnd.Next(0, 2);
+    Console.WriteLine(matr[i]);
+}
+Console.WriteLine();
+for(int i = 0; i < matr.Length; i ++){
+    if(matr[i] == 1){
+        matr[i] = 0;
+    }
+    else{matr[i] = 1;}
+    Console.WriteLine(matr[i]);
+}
