7b9856c2d906cb8fcd2307700ced8815dbcf1d58
Kirill <forestblackone@gmail.com>
2022-12-01 22:33:55 +0300

    37 great

---
+++ 37/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 37 Задать массив, заполнить случайными положительными трёхзначными числами. 
+// Показать количество нечетных\четных чисел
+
+int[] matr = new int[10];
+int n = 0;
+int j = 0;
+Random rnd = new Random();
+for(int i = 0; i < matr.Length; i ++){
+    matr[i] = rnd.Next(100, 1000);
+    Console.WriteLine(matr[i] + ";");
+    if(matr[i] % 2 == 0){
+        n++;
+    }
+    else{j++;}
+}
+Console.WriteLine("Четных: " + n);
+Console.WriteLine("Нечетных: " + j);
