4438aca6b7f9358a693898e87c24939e87fa9b2e
Kirill <forestblackone@gmail.com>
2022-12-07 22:24:44 +0300

    60 great

---
+++ 60/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 60 Показать двумерный массив размером m×n заполненный целыми случайными числами
+
+int[,] DoubleArray()
+{
+    int[,] array = new int[2, 5];
+    int rows = array.GetUpperBound(0) + 1;    // количество строк
+    int columns = array.Length / rows;
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = rnd.Next(0, 10);
+            Console.Write($"{array[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+DoubleArray();
