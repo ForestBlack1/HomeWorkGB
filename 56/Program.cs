d931ecde91f2eda782204175f6011f80aad7b0cc
Kirill <forestblackone@gmail.com>
2022-12-07 22:05:15 +0300

    56 great

---
+++ 56/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 56 Написать программу копирования массива
+
+int[] NewArray()
+{
+    Console.WriteLine("Создаем новый массив: ");
+    int[] newList = new int[10];
+    Random rnd = new Random();
+    for (int i = 0; i < newList.Length; i++)
+    {
+        newList[i] = rnd.Next(0, 1000);
+        Console.Write(newList[i] + " ");
+    }
+    Console.WriteLine();
+    return newList;
+}
+
+int[] CopyArray(int[] list)
+{
+    Console.WriteLine("Делаю копию массива...");
+    int[] newList = new int[list.Length];
+    for (int i = 0; i < newList.Length; i++)
+    {
+        newList[i] = list[i];
+    }
+    return newList;
+}
+
+void Result(int[] copyList)
+{
+    Console.WriteLine("Копия массива: ");
+    for (int i = 0; i < copyList.Length; i++)
+    {
+        Console.Write(copyList[i] + " ");
+    }
+}
+
+Result(CopyArray(NewArray()));
