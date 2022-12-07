db2d6c3fe16085521ae91da3e2f64ade34aea17a
Kirill <forestblackone@gmail.com>
2022-12-07 21:20:14 +0300

    51 great

---
+++ 51/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 51 С клавиатуры вводится число N. Затем вводятся N чисел. 
+// Определить сколько чисел больше 0 введено с клавиатуры
+
+int Array()
+{
+    Console.WriteLine("Введите число: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] list = new int[n];
+    int value = 0;
+    for (int i = 0; i < list.Length; i++)
+    {
+        Console.WriteLine("Введите число:");
+        list[i] = Convert.ToInt32(Console.ReadLine());
+        if (list[i] > 0) { value++; }
+    }
+    return value;
+}
+Console.WriteLine(Array());
