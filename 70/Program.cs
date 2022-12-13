4482af6f4b43ffffb18847ce5c6703c1d69ba17e
Kirill <forestblackone@gmail.com>
2022-12-13 21:17:54 +0300

    70 great

---
+++ 70/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 70 Показать натуральные числа от 1 до N, N задано
+int n = 7;
+int Rekurs(int a)
+{
+    Console.WriteLine(a);
+    if (a == 1) { return 1; }
+    return Rekurs(a - 1);
+}
+Rekurs(n);
