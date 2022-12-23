c94a03511a2bcb4ba7f23d1ecd6e6bbd2c3e0392
Kirill <forestblackone@gmail.com>
2022-12-23 21:02:12 +0300

    84 great

---
+++ 84/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 84 Определить являются ли введенные с клавиатуры символы правильно записью целого числа. 
+// Вычислить сумму цифр введенного числа
+
+Console.WriteLine("Введите текст: ");
+string? text = Console.ReadLine();
+
+int Main(string text)
+{
+    int count = 0;
+    char s = ' ';
+    for (int i = 0; i < text.Length; i++)
+    {
+        s = text[i];
+        if (Char.IsDigit(s))
+        {
+            count++;
+        }
+    }
+    if (count == text.Length)
+    {
+        count = 0;
+        for (int i = 0; i < text.Length; i++)
+        {
+            count += Convert.ToInt32(text[i].ToString());
+        }
+        return count;
+    }
+    else
+    {
+        Console.WriteLine("Запись не является целым числом");
+        return 0;
+    }
+
+}
+Console.WriteLine(Main(text));
