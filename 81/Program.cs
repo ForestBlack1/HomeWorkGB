6a1eed588bfe1d84df4c90c3f8bfee4ce6f2419a
Kirill <forestblackone@gmail.com>
2022-12-23 20:13:07 +0300

    81 great

---
+++ 81/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 81 Вывести на экран символы от A до Z как показано ниже: 
+//A 
+//BB 
+//CCC 
+//...
+string n = string.Empty;
+void Main(string v)
+{
+    int i = 1;
+    for (char a = 'A'; a <= 'Z'; a++)
+    {
+        n = new string(a, i);
+        Console.WriteLine(n);
+        i++;
+    }
+}
+Main(n);
