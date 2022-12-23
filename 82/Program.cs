0fd798096f156f44da96188c7a559a5e9353778f
Kirill <forestblackone@gmail.com>
2022-12-23 20:43:00 +0300

    82 great

---
+++ 82/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 82 C клавиатуры вводится строка разделенная точкой. Подсчитать количество символов до точки
+Console.WriteLine("Введите текст из двух предложений: ");
+string? text = Console.ReadLine();
+
+int Main(string text)
+{
+    int count = 0;
+    char n = '.';
+    for (int i = 0; i <= text.Length; i++)
+    {
+        if (text[i].ToString() == n.ToString())
+        {
+            return count;
+        }
+        count++;
+    }
+    return count;
+}
+Console.WriteLine(Main(text));
