02ab66f037cabc29c88a23bd310068a58b22acda
Kirill <forestblackone@gmail.com>
2022-12-23 20:52:02 +0300

    83 great

---
+++ 83/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 83 Подсчитать количество цифр среди вводимых с клавиатуры символов
+
+Console.WriteLine("Введите текст: ");
+string? text = Console.ReadLine();
+
+int Main(string text)
+{
+    int count = 0;
+    char s = ' ';
+    for (int i = 0; i < text.Length; i++)
+    {
+        s = text[i];
+        if (Char.IsDigit(s))
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine(Main(text));
