d7f339a71852a63ca813d79c75faa0cf49d1b1ed
Kirill <forestblackone@gmail.com>
2022-12-23 19:09:29 +0300

    80 great

---
+++ 80/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 80 Вывести на экран кодировку символов по ASCII(с 32 до 122)
+
+
+void Main()
+{
+    for (int i = 32; i <= 122; i++)
+    {
+        Console.WriteLine((char)i);
+    }
+}
+Main();
